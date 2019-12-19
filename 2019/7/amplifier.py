8949123ec6189273f8ba5693190c4d3c8fc3a52e
Sean Perry <shalehperry@gmail.com>
2019-12-18 16:07:33 -0800

    refactor into run one

--- 2019/7/amplifier.py
+++ 2019/7/amplifier.py
@@ -7,20 +7,23 @@ from io import StringIO
 from intcode import run_evaluate, parse
 
 
+def run_one_amplifier(input, setting, initial_value):
+    pipe_stdin = StringIO('\n'.join([str(setting), str(initial_value)]) + '\n')
+    pipe_stdout = StringIO()
+
+    run_evaluate(copy.copy(input), {'instruction_pointer': 0},
+                 stdin=pipe_stdin,
+                 stdout=pipe_stdout)
+
+    return pipe_stdout.getvalue().split()[-1]
+
+
 def run_iteration(pattern, input):
     value = 0
     print(pattern)
 
     for setting in pattern:
-        # print("Using:", setting, value)
-        pipe_stdin = StringIO('\n'.join([str(setting), str(value)]) + '\n')
-        pipe_stdout = StringIO()
-
-        run_evaluate(copy.copy(input), {'instruction_pointer': 0},
-                     stdin=pipe_stdin,
-                     stdout=pipe_stdout)
-
-        value = pipe_stdout.getvalue().split()[-1]
+        value = run_one_amplifier(input, setting, value)
 
     return int(value)
 
