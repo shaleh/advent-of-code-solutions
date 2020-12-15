aab1b35d0d10d4cf2456375463d3d8a0ba65f687
Sean Perry <shalehperry@gmail.com>
2020-12-14 16:39:47 -0800

    Round 2.

--- 2020/2/src/main.rs
+++ 2020/2/src/main.rs
@@ -1,20 +1,26 @@
 use std::convert::TryInto;
-use core::ops::RangeInclusive;
 use std::io::{self, BufRead};
 
 #[derive(Clone, Debug)]
 struct Policy {
-    range: RangeInclusive<u32>,
+    pos1: u32,
+    pos2: u32,
     letter: char,
 }
 
 impl Policy {
     fn new(raw_range: &str, letter: &str) -> Self {
         let range_values: Vec<u32> = raw_range.split("-").map(|x| x.parse().unwrap()).collect();
-        let begin = range_values.first().unwrap();
-        let end = range_values.last().unwrap();
-        Self { range: (*begin..=*end), letter: letter.chars().next().unwrap() }
+        let pos1 = *range_values.first().unwrap();
+        let pos2 = *range_values.last().unwrap();
+        dbg!(pos1, pos2, letter);
+        Self { pos1, pos2, letter: letter.chars().next().unwrap() }
     }
+}
+
+
+fn char_matches(c: Option<char>, to_match: char) -> i32 {
+    c.map_or(0, |c| if c == to_match { 1 } else {0})
 }
 
 #[derive(Clone, Debug)]
@@ -23,6 +29,15 @@ struct Password(String);
 impl Password {
     fn new(source: String) -> Self {
         Self(source)
+    }
+
+    fn is_valid(&self, policy: &Policy) -> bool {
+        let pos1: usize = policy.pos1.try_into().unwrap();
+        let pos2: usize = policy.pos2.try_into().unwrap();
+        let char1 = self.0.chars().nth(pos1 - 1);
+        let char2 = self.0.chars().nth(pos2 - 1);
+        let result = char_matches(char1, policy.letter) + char_matches(char2, policy.letter);
+        result == 1
     }
 }
 
@@ -42,18 +57,7 @@ fn read_input() -> Vec<(Policy, Password)> {
     input
 }
 
-fn honors_policy(password: &Password, policy: &Policy) -> bool {
-    let char_count: u32 = password.0.chars().filter(|c| *c == policy.letter).count().try_into().unwrap();
-    let result = policy.range.contains(&char_count);
-    if result {
-        result
-    } else {
-        dbg!(char_count, password, policy);
-        result
-    }
-}
-
 fn main() {
-    let match_count = read_input().iter().filter(|(policy, password)| honors_policy(&password, &policy)).count();
+    let match_count = read_input().iter().filter(|(policy, password)| password.is_valid(&policy)).count();
     println!("Matches policy: {}", match_count);
 }
