19deabd83623933c95aa5ffcc0ba48e171d03b57
Sean Perry <shalehperry@gmail.com>
2021-01-05 00:12:31 -0800

    day4

---
+++ 2015/day4/src/bin/day4a.rs
@@ -0,0 +1,26 @@
+use md5::{Md5, Digest};
+
+use advent_support::read_input;
+
+fn main() {
+    let lines = read_input::<String>().expect("Invalid input");
+    let base = lines[0].clone();
+
+    let mut current: i64 = 1;
+    loop {
+        let mut hasher = Md5::new();
+
+        let value = format!("{}{}", base, current);
+        dbg!(&value);
+        hasher.update(value);
+
+        let digest = hasher.finalize();
+        let hex_digest = format!("{:x}", digest);
+        if &hex_digest[0..5] == "00000" {
+            break;
+        }
+
+        current += 1;
+    }
+    println!("Value: {}", current);
+}
